d2c0b92284c518eb1a1f0a560976ce1da5936c55
PAISAN YOMYART <paisan.y@rbru.ac.th>
2026-03-27 09:49:25 +0700

    v2.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>HELLO GITHUB v2.0.0</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
